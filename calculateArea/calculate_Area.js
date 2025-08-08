0473534bb610bf817f856d595b2216210c5a382a
Seeeks <okarrdess@yahoo.com>
2025-08-08 04:44:07 -0400

    Practice task grocery sum

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let v1;
+let v2;
+let v3;
+
+function calculateGroceryTotal() {
+    v1 = parseFloat(document.getElementById('grocery1').value);
+    v2 = parseFloat(document.getElementById('grocery2').value);
+    v3 = parseFloat(document.getElementById('grocery3').value);
+
+    let sum = v1 + v2 + v3;
+    document.getElementById('resultG').innerText = `The total amount is :$ ${sum}`;
+}
