9028a8f89c75bdb0d0c18548f711fed36abb0475
Seeeks <okarrdess@yahoo.com>
2025-08-25 12:31:15 -0400

    Practice task: splice to remove entry

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -29,6 +29,7 @@ function showbooks() {
     <p><strong>Book Description: </strong>${book.bookDescription}</p>
     <p><strong>No. of Pages: </strong>${book.pagesNumber} page(s)</p>
     <button onclick="editbook(${index})">Edit</button>
+    <button onclick="deletebook(${index})">Delete</button>
     `);
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -49,4 +50,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deletebook(index) {
+    books.splice(index, 1);
+    showbooks();
 }
