1375861f9031474cfa6e8d62f1924b69596a7b01
Seeeks <okarrdess@yahoo.com>
2025-08-28 13:08:07 -0400

    Perform add, multiply, divide and allow non-numeric input to trigger message

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,23 +3,53 @@ function performOperation() {
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
     // Check if inputs are valid numbers
+
+    resetFields();
+
     if (!isNaN(num1) && !isNaN(num2)) {
     // Perform the operation
-                    let result = multiply(num1, num2);
-                    // Display the result
-                    displayResult(result);
-                } else {
-                    displayResult('Please enter valid numbers');
-                }
-            }
-            function multiply(a, b) {
-                // Introduce a debugger statement to pause execution
-                debugger;
-                // Multiply the numbers
-                return a * b;
-            }
-            function displayResult(result) {
-                // Display the result in the paragraph element
-                const resultElement = document.getElementById('result');
-                resultElement.textContent = `The result is: ${result}`;
-            }
+            let result1 = add(num1, num2);
+            displayResult(result1, "add");
+
+            let result2 = multiply(num1, num2);
+            // Display the result
+            displayResult(result2, "multiply");
+
+            let result3 = divide(num1, num2);
+
+            displayResult(result3, "divide");
+        } else {
+            displayResult('Please enter valid numbers', "add");
+        }
+
+    }
+    function multiply(a, b) {
+        
+        // Multiply the numbers
+        return a * b;
+    }
+
+    function add(a, b) {
+        // Introduce a debugger statement to pause execution
+        debugger;
+        return a + b;
+    }
+
+    function divide(a, b) {
+        return a / b;
+    }
+    function displayResult(result, operation) {
+        const fieldname = operation + "Result";
+        // Display the result in the paragraph element
+        const resultElement = document.getElementById(fieldname);
+        resultElement.textContent = `The ${operation} result is: ${result}`;
+    }
+
+    function resetFields() {
+        const fieldnames = ["addResult", "multiplyResult", "divideResult"];
+        fieldnames.forEach((fieldname) => {
+            const resultElement = document.getElementById(fieldname);
+            resultElement.textContent = "";
+        })
+        
+    }
